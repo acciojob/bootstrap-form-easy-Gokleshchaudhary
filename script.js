7c5c27780d2609da928ba66445bdfafb908ff539
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-05-24 08:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+document.getElementById('internForm').addEventListener('submit', function(event) {
+        if (!this.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        this.classList.add('was-validated');
+    });
